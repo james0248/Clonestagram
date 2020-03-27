29ef56ff87ccfa5cdb3e0940ab82c4f68d802632
james0248 <hsjames0248@gmail.com>
2020-03-27 16:42:42 +0900

    Add Facebook login feature

--- app/src/main/java/com/example/clonestagram/LoginActivity.kt
+++ app/src/main/java/com/example/clonestagram/LoginActivity.kt
@@ -4,37 +4,99 @@ import android.content.Intent
 import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.facebook.AccessToken
 import com.google.firebase.auth.FirebaseAuth
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_login.btn_login_facebook
 import kotlinx.android.synthetic.main.activity_login.txt_password
-import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var callBackManager : CallbackManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        callBackManager = CallbackManager.Factory.create()
         initColors()
 
         link_signup.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SignupActivity::class.java))
         }
+        btn_login_email.setOnClickListener {
+            login()
+        }
+        btn_login_facebook.setOnClickListener {
+            facebookLogin()
+        }
+    }
+
+    private fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callBackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onError(error: FacebookException?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCancel() {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+
+    private fun handleFacebookAccessToken(token : AccessToken?) {
+        val credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "Login Success!", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callBackManager?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun connect() {
+        val uri = MongoClientURI(System.getenv("MONGO_URI"))
+        val mongoClient = MongoClient(uri)
+        val users = mongoClient.getDatabase("account")
     }
 
     private fun login() {
         auth?.signInWithEmailAndPassword(txt_login.text.toString(), txt_password.text.toString())
-            ?.addOnCompleteListener {
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "Login Success!", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
             }
-
-    }
-
-    private fun moveMainPage() {
-        startActivity(Intent(this, MainActivity::class.java))
     }
 
     private fun initColors() {
--- app/src/main/java/com/example/clonestagram/SignupActivity.kt
+++ app/src/main/java/com/example/clonestagram/SignupActivity.kt
@@ -1,17 +1,87 @@
 package com.example.clonestagram
 
+import android.content.Intent
 import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.android.synthetic.main.activity_signup.btn_login_facebook
+import kotlinx.android.synthetic.main.activity_signup.txt_password
+import java.util.*
 
 class SignupActivity : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
+    private var callBackManager : CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        auth = FirebaseAuth.getInstance()
+        callBackManager = CallbackManager.Factory.create()
         initColors()
+
+        btn_signup.setOnClickListener {
+            signupWithEmail()
+        }
+        btn_login_facebook.setOnClickListener {
+            facebookLogin()
+        }
+    }
+
+    private fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callBackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onError(error: FacebookException?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCancel() {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+
+    private fun handleFacebookAccessToken(token : AccessToken?) {
+        val credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "Signup with Facebook success!", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun signupWithEmail() {
+        auth?.createUserWithEmailAndPassword(txt_email.text.toString(), txt_password.text.toString())
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "Signup Successful!", Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     private fun initColors() {
